40a70c5e2fc73e7a7819f9d4fbbfcdeeab1e8748
Stefan Freitag <stefan.freitag@udo.edu>
2016-01-05 21:24:26 +0100

    Added initial code for location provider enabling

--- app/src/main/java/de/freitag/stefan/android/geolocation/MainActivity.java
+++ app/src/main/java/de/freitag/stefan/android/geolocation/MainActivity.java
@@ -1,18 +1,35 @@
 package de.freitag.stefan.android.geolocation;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStates;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+
 import de.freitag.stefan.android.R;
 
 public final class MainActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
@@ -56,20 +73,96 @@ public final class MainActivity extends Activity implements GoogleApiClient.Conn
     private TextView viewAccuracy;
 
 
+
     @Override
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.main);
         this.bindTextViews();
         this.buildGoogleApiClient();
-        this.mLocationRequest =  this.createLocationRequest();
+
+        this.mLocationRequest = this.createLocationRequest();
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient, builder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                final LocationSettingsStates states = result.getLocationSettingsStates();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        // All location settings are satisfied. The client can initialize location
+                        // requests here.
+                        //...
+                        Log.i(TAG, "XXXXXXXXXXXXXXx");
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        // Location settings are not satisfied. But could be fixed by showing the user
+                        // a dialog.
+                        Log.i(TAG, "ZZZZZZZZZZZZZZZZZZZ");
+
+
+                        // Location settings are not satisfied. But could be fixed by showing the user
+                        // a dialog.
+                        try {
+                            // Show the dialog by calling startResolutionForResult(),
+                            // and check the result in onActivityResult().
+                            status.startResolutionForResult(
+                                    MainActivity.this,
+                                    CommonStatusCodes.RESOLUTION_REQUIRED);
+                        } catch (IntentSender.SendIntentException e) {
+                            // Ignore the error.
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        // Location settings are not satisfied. However, we have no way to fix the
+                        // settings so we won't show the dialog.
+                        Log.i(TAG, "YYYYYYYYYYYYYYYYYYYY");
+                        break;
+                }
+            }
+        });
+
+
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        final LocationSettingsStates states = LocationSettingsStates.fromIntent(data);
+        switch (requestCode) {
+            case CommonStatusCodes.RESOLUTION_REQUIRED:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        Log.i(TAG, "Result ok");
+                        // All required changes were successfully made
+        //                ...
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Log.i(TAG, "Result canceled.");
+                        // The user was asked to change settings, but chose not to
+          //              ...
+                        break;
+                    default:
+                        break;
+                }
+                break;
+        }
+    }
+
+    /**
+     * Bind the resources to local fields.
+     */
     private void bindTextViews() {
         this.viewLatitude = (TextView) findViewById(R.id.TextView_Latitude);
+        assert this.viewLatitude != null;
         this.viewLongitude = (TextView) findViewById(R.id.TextView_Longitude);
+        assert this.viewLongitude != null;
         this.viewProvider = (TextView) findViewById(R.id.TextView_Provider);
+        assert this.viewProvider != null;
         this.viewAccuracy = (TextView) findViewById(R.id.TextView_Accuracy);
+        assert this.viewAccuracy != null;
     }
 
     /**
@@ -124,6 +217,7 @@ public final class MainActivity extends Activity implements GoogleApiClient.Conn
     protected void onStop() {
         this.mGoogleApiClient.disconnect();
         super.onStop();
+
     }
 
 
